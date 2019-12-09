5eda56e7d4115eb629ceb202b173899dfb9827b7
ksnehanjali <ksnehanjali@gmail.com>
2019-12-09 13:44:05 +0530

    Add edit show and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end  
+
   def new
     @article = Article.new
   end
-  
+ 
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -14,6 +18,21 @@ class ArticlesController < ApplicationController
       render 'new'
     end    
   end
+
+  def edit 
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end  
+      
+  end  
 
   def show
     @article = Article.find(params[:id])
